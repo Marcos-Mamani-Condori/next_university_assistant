b8d713b3a15ed47f0ddea67c04a30eb433f9b5ed
marcosrmc3 <mamanimarcos113@gmail.com>
2024-10-24 16:27:40 -0400

    conflictos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,11 @@
 'use client';
-import CircularRadiusExam from "@/components/CircularRadiusExam"; // Usa la ruta correcta
+
+import { useState } from "react";
+import CircularRadiusExam from "@/components/CircularRadiusExam";
 import StudentsCounter from "@/components/StudentsCounter";
 import Announcements from "@/components/Announcements";
-import { useState } from "react";
-//import { useContext } from "react";
-//import ModalContext from "@/context/ModalContext";
 import ActivityCalendar from "@/components/ActivityCalendar";
 
-/////////////////////////////////////////////////////////////////
 const activities = [
     { day: "20", month: "Oct", title: "Aniversario de la UNIVERSIDAD LOYOLA" },
     { day: "28", month: "Oct", title: "Exámenes 1er Ciclo UAM-4" },
@@ -15,48 +13,37 @@ const activities = [
     { day: "04", month: "Nov", title: "Inicio 2° Ciclo UAM-4" },
     { day: "25", month: "Nov", title: "Exámenes 2° Ciclo UAM-4" },
 ];
-/////////////////////////////////////////////////////////////////
-
 
 const Home = () => {
     // Obtiene la fecha actual
     const now = new Date();
-    //const { isRegisterModalOpen, setIsRegisterModalOpen } = useContext(ModalContext);causas confusiones con otro componente y hace que se abra
-    //crear otro contexto o estado
-    const [ModalOpen,setIsModalOpen]=useState(false);
+    
+    // Estado para el modal
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
     return (
-        //<div className={`${isRegisterModalOpen ? "blur-sm" : ""}`}></div>//
-        // Con (mx-2 md:mx-10 lg:mx-[10rem]) hacemos la responsividad en modo móvil, escritorio y tablet
         <div>
-            <Announcements isModalOpen={ModalOpen} setModalOpen={setIsModalOpen}/>
-                <div className={`flex flex-row mx-2 md:mx-10 lg:mx-[10rem] space-x-4 ${ModalOpen ? "blur-sm" : ""}`}>
-                    <div className="flex-1">
-                        <StudentsCounter />
-                    </div>
-                    <div className="flex-1">
-                        <CircularRadiusExam
-                            startDate={'2024-09-01'}
-                            dateExam={'2024-09-30'}
-                            fechaActual={now}
-                        />
-                    </div>
-                </div>
+            <Announcements isModalOpen={isModalOpen} setModalOpen={setIsModalOpen} />
             
-        <div className="flex flex-row mx-2 md:mx-10 lg:mx-[10rem] space-x-4">
-            <div className="flex-1">
-                <StudentsCounter />
+            <div className={`flex flex-row mx-2 md:mx-10 lg:mx-[10rem] space-x-4 ${isModalOpen ? "blur-sm" : ""}`}>
+                <div className="flex-1">
+                    <StudentsCounter />
+                </div>
+                
             </div>
 
-            {/* Alineado hacia arriba en pantallas grandes (escritorio) */}
-            <div className="col-span-2 md:col-span-1 lg:col-span-1 flex items-center lg:items-start justify-center">
-                <CircularRadiusExam
-                    startDate={'2024-10-17'}
-                    dateExam={'2024-10-29'}
-                    fechaActual={now}
-                />
-            </div>
-            <div className="flex-1">
-                <ActivityCalendar activities={activities} /> 
+            <div className="flex flex-row mx-2 md:mx-10 lg:mx-[10rem] space-x-4">
+                {/* Alineado hacia arriba en pantallas grandes (escritorio) */}
+                <div className="flex-1">
+                    <ActivityCalendar activities={activities} /> 
+                </div>
+                <div className="col-span-2 md:col-span-1 lg:col-span-1 flex items-center lg:items-start justify-center">
+                    <CircularRadiusExam
+                        startDate={'2024-10-17'}
+                        dateExam={'2024-10-29'}
+                        fechaActual={now}
+                    />
+                </div>
             </div>
         </div>
     );
