503627b8bcb510a5d1472406ab2eb63918ef64b6
marcosrmc3 <mamanimarcos113@gmail.com>
2024-11-15 19:10:10 -0400

    quitando logs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-
 ///** @type {import('next').NextConfig} */
-//const nextConfig = {
-    //eslint: {
-   //   ignoreDuringBuilds: true,  // Ignorar ESLint durante el proceso de build
-   // },
-  //};
+//const nextConfig = {};
+
+//export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+eslint: {
+   ignoreDuringBuilds: true,  // Ignorar ESLint durante el proceso de build
+ },
+};
   
-  //export default nextConfig;
+export default nextConfig;
   
