b8aad2e17c7d68538d6f4cd6edd805e48d4523db
marcosrmc3 <mamanimarcos113@gmail.com>
2024-11-15 18:39:41 -0400

    mejorando modoo produccion

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+///** @type {import('next').NextConfig} */
+//const nextConfig = {
+    //eslint: {
+   //   ignoreDuringBuilds: true,  // Ignorar ESLint durante el proceso de build
+   // },
+  //};
+  
+  //export default nextConfig;
+  
